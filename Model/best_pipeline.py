be47adadc40292d03df9372a281b807ea1df5e21
root <root@49990a2b8350>
2022-04-04 21:32:29 +0000

    Model Deployment

--- Model/best_pipeline.py
+++ Model/best_pipeline.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -9,8 +9,8 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: 0.975
-exported_pipeline = LogisticRegression(C=15.0, dual=False, penalty="l2")
+# Average CV score on the training set was: 0.9515656001891349
+exported_pipeline = SGDClassifier(alpha=0.01, eta0=1.0, fit_intercept=True, l1_ratio=0.5, learning_rate="invscaling", loss="hinge", penalty="elasticnet", power_t=0.1)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
